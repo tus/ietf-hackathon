a992eb491b1c20a12409659cb185e376021327d3
Piotrek Zadroga <piotrek@isc.org>
2023-11-05 14:19:12 +0100

    add example HEAD tests where 400 response is expected

--- tests/test_rufh.py
+++ tests/test_rufh.py
@@ -85,6 +85,48 @@ class TestHead:
         assert UPLOAD_COMPLETE in r.headers
         assert r.headers[UPLOAD_COMPLETE] == TRUE
 
+    def test_offset_retrieval_bad_head(self, post_with_file, session, head):
+        post_with_file.headers[UPLOAD_COMPLETE] = TRUE
+
+        r = session.send(post_with_file)
+        assert r.status_code == 104, "104 status code was expected"
+        assert LOCATION in r.headers, "Location header was expected"
+        assert r.headers[LOCATION], "Non-empty Location header value was expected"
+
+        location = r.headers[LOCATION]
+        # urlparse will throw an exception in case the location url is not correct
+        # to pass the test, we don't expect the exception
+        urlparse(location)
+        head.url = location
+        # The request MUST NOT include an Upload-Offset
+        head.headers[UPLOAD_OFFSET] = str(10)
+        r = session.send(head)
+        # The request MUST NOT include an Upload-Offset or Upload-Complete header field.
+        # The server MUST reject requests with either of these fields by responding with
+        # a 400 (Bad Request) status code.
+        assert r.status_code == 400, "400 status code was expected"
+
+    def test_offset_retrieval_bad_head2(self, post_with_file, session, head):
+        post_with_file.headers[UPLOAD_COMPLETE] = TRUE
+
+        r = session.send(post_with_file)
+        assert r.status_code == 104, "104 status code was expected"
+        assert LOCATION in r.headers, "Location header was expected"
+        assert r.headers[LOCATION], "Non-empty Location header value was expected"
+
+        location = r.headers[LOCATION]
+        # urlparse will throw an exception in case the location url is not correct
+        # to pass the test, we don't expect the exception
+        urlparse(location)
+        head.url = location
+        # The request MUST NOT include an Upload-Complete
+        head.headers[UPLOAD_COMPLETE] = FALSE
+        r = session.send(head)
+        # The request MUST NOT include an Upload-Offset or Upload-Complete header field.
+        # The server MUST reject requests with either of these fields by responding with
+        # a 400 (Bad Request) status code.
+        assert r.status_code == 400, "400 status code was expected"
+
     def test_simple_head(self):
         pass
 
